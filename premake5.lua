a4050479d199ad3ef56413248a534da49c3a32de
polymonster <alexandercdixon@gmail.com>
2017-12-14 13:49:25 +0000

    initial setup and compiling

--- premake5.lua
+++ premake5.lua
@@ -1,42 +1,4 @@
---add extension options
-newoption 
-{
-   trigger     = "renderer",
-   value       = "API",
-   description = "Choose a renderer",
-   allowed = 
-   {
-      { "opengl", "OpenGL" },
-      { "dx11",  "DirectX 11 (Windows only)" },
-   }
-}
-
-newoption 
-{
-   trigger     = "sdk_version",
-   value       = "version",
-   description = "Specify operating system SDK",
-}
-
-newoption 
-{
-   trigger     = "platform_dir",
-   value       = "dir",
-   description = "specify platform specifc src folder",
-}
-
-newoption 
-{
-   trigger     = "xcode_target",
-   value       = "TARGET",
-   description = "Choose an xcode build target",
-   allowed = 
-   {
-      { "osx", "OSX" },
-      { "ios",  "iOS" },
-   }
-}
-
+dofile "../pmtech/tools/premake/options.lua"
 dofile "../pmtech/tools/premake/globals.lua"
 dofile "../pmtech/tools/premake/app_template.lua"
 
@@ -56,7 +18,7 @@ dofile "../pmtech/put/project.lua"
 
 -- Example projects	
 -- ( project name, current script dir, )
-create_app( "dr_scientist", script_path() )
+create_app( "dr_scientist", "", script_path() )
 
 	
 	
