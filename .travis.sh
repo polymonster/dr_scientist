552be3f68d16ab4927c26e1774aff76ac986dc05
Alex Dixon <alexandercdixon@gmail.com>
2019-02-07 16:03:55 +0000

    Delete .travis.sh

--- .travis.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd ..
-git clone --recursive https://github.com/polymonster/pmtech.git
-ls
-cd dr_scientist
-python3 ../pmtech/tools/build.py -code -libs -ide gmake -platform osx
-cd build/osx
-make Makefile all
